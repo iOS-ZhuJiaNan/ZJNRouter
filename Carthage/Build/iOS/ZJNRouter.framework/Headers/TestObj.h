33e671bb22b8634106e3953ac79159acbd656985
朱佳男 <754148169@qq.com>
2020-04-15 23:16:24 +0800

    这次总该成功了吧

---
+++ Carthage/Build/iOS/ZJNRouter.framework/Headers/TestObj.h
@@ -0,0 +1,17 @@
+//
+//  TestObj.h
+//  ZJNRouter
+//
+//  Created by 谦卦 on 2020/4/15.
+//  Copyright © 2020 ZJN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObj : NSObject
+-(void)log;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Carthage/Build/iOS/ZJNRouter.framework/Headers/ZJNRouter.h
@@ -0,0 +1,19 @@
+//
+//  ZJNRouter.h
+//  ZJNRouter
+//
+//  Created by 谦卦 on 2020/4/15.
+//  Copyright © 2020 ZJN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ZJNRouter/TestObj.h>
+//! Project version number for ZJNRouter.
+FOUNDATION_EXPORT double ZJNRouterVersionNumber;
+
+//! Project version string for ZJNRouter.
+FOUNDATION_EXPORT const unsigned char ZJNRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZJNRouter/PublicHeader.h>
+
+
